a084d5747657ca8ab5a2e1fc9a78b3068a87cc18
Amir <amiirr62@yahoo.com>
2023-06-20 19:22:49 -0700

    Passport Configs

---
+++ passport/passport-local.js
@@ -0,0 +1,64 @@
+const express = require('express')
+const passport = require('passport')
+const localStrategy = require('passport-local').Strategy
+User = require('../models/user')
+
+//Coding User's id and store in browser
+passport.serializeUser((user,done)=>{
+    done(null, user.id)
+
+})
+
+//Decoding Cookie and deliver user's id
+passport.deserializeUser(async (id,done)=>{
+    let user = await User.findById(id)
+    if(user) done(null,user)
+})
+//********************************* Register Strategy ***************************************** */
+passport.use("local.register", new localStrategy(
+    {
+        usernameField : 'email',
+        passwordField : 'password',
+        passReqToCallback : true,
+    }, async(req, email, password, done)=>{
+        try {
+            let user = await User.findOne({email : req.body.email})
+            if (user){
+                return done(null, false, req.flash('errors','User already existed.!!'))
+            }
+            const newUser = new User({
+                name : req.body.name,
+                email: req.body.email,
+                password: req.body.password,
+            })
+
+            await newUser.save()
+            done(null, newUser)
+        } 
+        catch (error) {
+            return done(error, false, {message:error})
+        }
+
+    }
+))
+//********************************* Login Strategy ***************************************** */
+passport.use("local.login", new localStrategy(
+{
+    usernameField : 'email',
+    passwordField : 'password',
+    passReqToCallback : true,
+}, async(req, email, password, done)=>{
+   try {
+    let user = await User.findOne({email : req.body.email})
+
+    if(!user || user.password != req.body.password){
+        return done(null, false, req.flash('errors','No users have found!!!  '))
+    }
+
+    done(null,user)
+
+   } catch (error) {
+    return done(error, false, {message :  error})
+   }
+    
+        }))
--- server.js
+++ server.js
@@ -6,6 +6,7 @@ const flash = require('connect-flash')
 const cookieParser = require('cookie-parser')
 const session = require('express-session') 
 const mongoose = require('mongoose')
+const passport = require('passport')
 
 const app = express()
 require('dotenv').config()
@@ -29,6 +30,11 @@ app.use(session({
   }))
 
 app.use(flash())
+
+
+require('./passport/passport-local')
+app.use(passport.initialize())
+app.use(passport.session())
 
 app.use((req,res,next)=>{
   res.locals = {errors : req.flash('errors')}
